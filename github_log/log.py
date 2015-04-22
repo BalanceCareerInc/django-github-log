07d288b1da3083b25dcc156fc61c27a4757ea54a
Acuros <acuros@kaist.ac.kr>
2015-04-22 12:49:33 +0900

    Circular reference resolved

--- github_log/log.py
+++ github_log/log.py
@@ -1,6 +1,5 @@
 import logging
 
-from github_log.models import ErrorLog
 from github_log.tasks import shoot_to_github
 
 
@@ -8,5 +7,6 @@ class GitHubIssueHandler(logging.Handler):
     """Register error as an github issue"""
 
     def emit(self, record):
+        from github_log.models import ErrorLog
         log = ErrorLog.create_from_record(record)
         shoot_to_github.delay(log)
